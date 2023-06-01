ec8d93cc415fc496cdfb21e62fd08ce6936efbe9
zhuyuhan2333 <zhuyuhan2333@qq.com>
2023-06-01 15:53:38 +0800

    optimize: optimize some codes

--- random_walk_failure_instance/workflow.py
+++ random_walk_failure_instance/workflow.py
@@ -17,131 +17,6 @@ from random_walk_failure_instance.config import RandomWalkFailureInstanceConfig
 from random_walk_failure_instance.model import random_walk
 
 
-# def workflow(config: RandomWalkFailureInstanceConfig):
-#     logger.info(
-#         f"\n================================================Config=============================================\n"
-#         f"{config!s}"
-#         f"\n===================================================================================================\n"
-#     )
-#     logger.info(f"reproducibility info: {config.get_reproducibility_info()}")
-#     logger.add(config.output_dir / 'log')
-#     config.save(str(config.output_dir / "config"))
-#
-#     base = FDGModelInterface(config)
-#     fdg = base.fdg
-#     mp = base.metric_preprocessor
-#
-#     y_trues = []
-#     y_preds = []
-#     dimension = [418, 496, 159]
-#
-#     with profile("random_walk_main", report_printer=lambda _: logger.info(f"\n{_}")) as profiler:
-#         folder = "/Users/zhuyuhan/Documents/391-WHU/experiment/researchProject/MicroIRC/data/data2/1"
-#         minute = 10
-#         # time_data
-#         metric_source_data = pd.read_csv(folder + '/' + 'metric.norm.csv')
-#         metric_source_data = metric_source_data.fillna(0)
-#         time_data = metric_source_data.iloc[:,0]
-#
-#         # read root_causes
-#         label_file_name = folder + '/' + 'label.csv'
-#         label_data = pd.read_csv(label_file_name, encoding='utf-8')
-#         label_set = set()
-#         label_map = {}
-#         for index, raw in label_data.iterrows():
-#             label_set.add(raw['cmdb_id'] + '&' + raw['fault_description'])
-#         label_list = sorted(list(label_set))
-#         for label in list(label_set):
-#             label_map[label] = label_list.index(label)
-#         class_num = len(label_set)
-#         root_causes = label_data['cmdb_id']
-#
-#         time_list = []
-#
-#         for row in label_data.itertuples():
-#             root_cause = row[4]
-#             root_cause_level = row[6]
-#             # real_time = label_list[i] + row[1]
-#             # real_timestamp = int(time.mktime(time.strptime(real_time, "%Y-%m-%d %H:%M:%S")))
-#             real_timestamp = int(row[2])
-#             # begin_timestamp = real_timestamp - 30 * minute
-#             begin_timestamp = real_timestamp - 60 * minute
-#             # end_timestamp = real_timestamp + 30 * minute
-#             end_timestamp = real_timestamp + 60 * minute
-#             failure_type = row[5]
-#             lb = label_map[root_cause + '&' + str(row[3])]
-#             t = Time(begin_timestamp, end_timestamp, root_cause, root_cause_level, failure_type, lb)
-#             time_list.append(t)
-#         # for row in label_data.itertuples():
-#         #     root_cause = row[3]
-#         #     root_cause_level = row[6]
-#         #     # real_time = label_list[i] + row[1]
-#         #     # real_timestamp = int(time.mktime(time.strptime(real_time, "%Y-%m-%d %H:%M:%S")))
-#         #     real_timestamp = int(row[2])
-#         #     begin_timestamp = real_timestamp - 30 * minute
-#         #     end_timestamp = real_timestamp + 30 * minute
-#         #     failure_type = row[5]
-#         #     lb = label_map[root_cause + '&' + str(row[4])]
-#         #     t = Time(begin_timestamp, end_timestamp, root_cause, root_cause_level, failure_type, lb)
-#         #     time_list.append(t)
-#         graphsage = trainGraphSage(time_list, class_num, None, dimension[0])
-#         nums = []
-#         acc_count = 0
-#         acc = 0
-#         for fid in tqdm(base.test_failure_ids):
-#             cache_dir = Path("SSF/tmp/failure_instance_random_walk_cache") / config.data_dir.relative_to("SSF/") / f"{fid=}"
-#             logger.info(f"Cache dir: {cache_dir}")
-#             cache = Cache(
-#                 directory=str(cache_dir),
-#                 size_limit=int(1e10),
-#             )
-#             # 异常时间戳
-#             failure_ts = fdg.failure_at(fid)["timestamp"]
-#             # fdg图
-#             graph = fdg.networkx_graph_at(fid).copy()
-#             for failure_class, class_metrics in zip(
-#                     fdg.failure_classes, mp(failure_ts, window_size=config.window_size)
-#             ):
-#                 for failure_instance, instance_metrics in zip(
-#                         fdg.failure_instances[failure_class],
-#                         class_metrics,
-#                 ):
-#                     assert instance_metrics.shape == (fdg.metric_number_dict[failure_class], sum(config.window_size))
-#                     graph.add_node(failure_instance, values=instance_metrics.numpy())
-#             failure_instance_scores = random_walk(
-#                 graph,
-#                 config=config,
-#                 cache=cache,
-#             )
-#             failure_instance_scores = sorted(list(failure_instance_scores.keys()), key=lambda x: failure_instance_scores[x], reverse=True)
-#             time = None
-#             for t in time_list:
-#                 if t.is_in_time(failure_ts):
-#                     time = t
-#                     break
-#             if time is None:
-#                 continue
-#             if 'docker' not in time.root_cause and 'service' not in time.root_cause:
-#                 continue
-#             val = []
-#             for i in range(time.begin_index, time.end_index + 1): val.append(i)
-#             val_output = graphsage.forward(val, metric_source_data.iloc[:,2:].loc[val], is_node_train_index=False)
-#             classification = val_output.data.numpy().argmax(axis=1)
-#             failure_instance_scores_final = []
-#             for f in failure_instance_scores:
-#                 if 'os' not in f and 'db' not in f:
-#                     failure_instance_scores_final.append(f)
-#
-#             failure_instance_scores = {i: len(failure_instance_scores_final) - failure_instance_scores_final.index(i) for i in failure_instance_scores_final}
-#             failure_instance_scores = rank(failure_instance_scores, classification, label_map)
-#             acc_temp = my_acc_IRC(failure_instance_scores, [root_cause])
-#             if acc_temp > 0:
-#                 acc_count += 1
-#                 acc += acc_temp
-#             nums.append(count_rank(time.root_cause, sorted(list(failure_instance_scores.keys()), key=lambda x: failure_instance_scores[x], reverse=True)))
-#     print_pr(nums)
-#     print("Acc" + str(acc / acc_count))
-
 def workflow(config: RandomWalkFailureInstanceConfig):
     logger.info(
         f"\n================================================Config=============================================\n"
@@ -161,6 +36,58 @@ def workflow(config: RandomWalkFailureInstanceConfig):
     dimension = [418, 496, 159]
 
     with profile("random_walk_main", report_printer=lambda _: logger.info(f"\n{_}")) as profiler:
+        folder = "/Users/zhuyuhan/Documents/391-WHU/experiment/researchProject/MicroIRC/data/data2/1"
+        minute = 10
+        # time_data
+        metric_source_data = pd.read_csv(folder + '/' + 'metric.norm.csv')
+        metric_source_data = metric_source_data.fillna(0)
+        time_data = metric_source_data.iloc[:,0]
+
+        # read root_causes
+        label_file_name = folder + '/' + 'label.csv'
+        label_data = pd.read_csv(label_file_name, encoding='utf-8')
+        label_set = set()
+        label_map = {}
+        for index, raw in label_data.iterrows():
+            label_set.add(raw['cmdb_id'] + '&' + raw['fault_description'])
+        label_list = sorted(list(label_set))
+        for label in list(label_set):
+            label_map[label] = label_list.index(label)
+        class_num = len(label_set)
+        root_causes = label_data['cmdb_id']
+
+        time_list = []
+
+        for row in label_data.itertuples():
+            root_cause = row[4]
+            root_cause_level = row[6]
+            # real_time = label_list[i] + row[1]
+            # real_timestamp = int(time.mktime(time.strptime(real_time, "%Y-%m-%d %H:%M:%S")))
+            real_timestamp = int(row[2])
+            # begin_timestamp = real_timestamp - 30 * minute
+            begin_timestamp = real_timestamp - 60 * minute
+            # end_timestamp = real_timestamp + 30 * minute
+            end_timestamp = real_timestamp + 60 * minute
+            failure_type = row[5]
+            lb = label_map[root_cause + '&' + str(row[3])]
+            t = Time(begin_timestamp, end_timestamp, root_cause, root_cause_level, failure_type, lb)
+            time_list.append(t)
+        # for row in label_data.itertuples():
+        #     root_cause = row[3]
+        #     root_cause_level = row[6]
+        #     # real_time = label_list[i] + row[1]
+        #     # real_timestamp = int(time.mktime(time.strptime(real_time, "%Y-%m-%d %H:%M:%S")))
+        #     real_timestamp = int(row[2])
+        #     begin_timestamp = real_timestamp - 30 * minute
+        #     end_timestamp = real_timestamp + 30 * minute
+        #     failure_type = row[5]
+        #     lb = label_map[root_cause + '&' + str(row[4])]
+        #     t = Time(begin_timestamp, end_timestamp, root_cause, root_cause_level, failure_type, lb)
+        #     time_list.append(t)
+        graphsage = trainGraphSage(time_list, class_num, None, dimension[0])
+        nums = []
+        acc_count = 0
+        acc = 0
         for fid in tqdm(base.test_failure_ids):
             cache_dir = Path("SSF/tmp/failure_instance_random_walk_cache") / config.data_dir.relative_to("SSF/") / f"{fid=}"
             logger.info(f"Cache dir: {cache_dir}")
@@ -187,22 +114,33 @@ def workflow(config: RandomWalkFailureInstanceConfig):
                 cache=cache,
             )
             failure_instance_scores = sorted(list(failure_instance_scores.keys()), key=lambda x: failure_instance_scores[x], reverse=True)
-            y_trues.append(set(fdg.root_cause_instances_of(fid)))
-            y_preds.append(
-                sorted(list(failure_instance_scores.keys()), key=lambda x: failure_instance_scores[x], reverse=True))
+            time = None
+            for t in time_list:
+                if t.is_in_time(failure_ts):
+                    time = t
+                    break
+            if time is None:
+                continue
+            if 'docker' not in time.root_cause and 'service' not in time.root_cause:
+                continue
+            val = []
+            for i in range(time.begin_index, time.end_index + 1): val.append(i)
+            val_output = graphsage.forward(val, metric_source_data.iloc[:,2:].loc[val], is_node_train_index=False)
+            classification = val_output.data.numpy().argmax(axis=1)
+            failure_instance_scores_final = []
+            for f in failure_instance_scores:
+                if 'os' not in f and 'db' not in f:
+                    failure_instance_scores_final.append(f)
 
-    for y_true, y_pred in zip(y_trues, y_preds):
-        logger.info(
-            f"{';'.join(y_true):<30}"
-            f"|{', '.join(y_pred[:5]):<50}"
-        )
-    metrics = get_evaluation_metrics_dict(y_trues, y_preds, max_rank=fdg.n_failure_instances)
-    logger.info(format_result_string(
-        metrics,
-        profiler,
-        DejaVuConfig().from_dict(args_dict=config.as_dict(), skip_unsettable=True)
-    ))
-    return y_trues, y_preds
+            failure_instance_scores = {i: len(failure_instance_scores_final) - failure_instance_scores_final.index(i) for i in failure_instance_scores_final}
+            failure_instance_scores = rank(failure_instance_scores, classification, label_map)
+            acc_temp = my_acc_IRC(failure_instance_scores, [root_cause])
+            if acc_temp > 0:
+                acc_count += 1
+                acc += acc_temp
+            nums.append(count_rank(time.root_cause, sorted(list(failure_instance_scores.keys()), key=lambda x: failure_instance_scores[x], reverse=True)))
+    print_pr(nums)
+    print("Acc" + str(acc / acc_count))
 
 def trainGraphSage(time_list, class_num, label_file, dimension, train = False):
     # build svc call
